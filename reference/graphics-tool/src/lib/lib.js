52b809633a518aa8ab70bba033e008be0508b30e
Johan Nyman <johan@nytamin.se>
2025-01-01 19:35:05 +0100

    dist fix

--- reference/graphics-tool/src/lib/lib.js
+++ reference/graphics-tool/src/lib/lib.js
@@ -3,7 +3,10 @@ export function pathJoin(...paths) {
 }
 export function graphicResourcePath(...paths) {
 	// This URL prefix causes the service-worker to intercept the requests and serve the file from the local file system:
-	return pathJoin('http://LOCAL/', ...paths)
+
+	const prefix = window.location.href.includes('https') ? 'https://' : 'http://'
+
+	return pathJoin(`${prefix}LOCAL/`, ...paths)
 }
 export async function sleep(ms) {
 	await new Promise((resolve) => setTimeout(resolve, ms))
