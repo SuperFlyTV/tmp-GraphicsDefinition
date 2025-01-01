78cff4522834c7ffbc0627379350af8e895a406e
Johan Nyman <johan@nytamin.se>
2025-01-01 19:25:02 +0100

    graphics-tool: vite build options

--- reference/graphics-tool/vite.config.js
+++ reference/graphics-tool/vite.config.js
@@ -1,46 +1,47 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { buildSync } from "esbuild"
+import { buildSync } from 'esbuild'
 
 // import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
-    // resolve: {
-    //     alias: {
-    //       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    //     }
-    //   },
-    server: {
-        port: 8083
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: '[name][extname]',
-                chunkFileNames: '[name].js',
-                entryFileNames: '[name].js',
-            },
-        },
-        assetsDir: '',
-    },
-    base: '',
+	root: path.resolve(__dirname, 'src'),
+	// resolve: {
+	//     alias: {
+	//       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+	//     }
+	//   },
+	server: {
+		port: 8083,
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: '[name][extname]',
+				chunkFileNames: '[name].js',
+				entryFileNames: '[name].js',
+			},
+		},
+		assetsDir: '',
+		outDir: '../dist',
+	},
+	base: '',
 
-    plugins: [
-        // ...
-        react(),
-        {
-          apply: "build",
-          enforce: "post",
-          transformIndexHtml() {
-            buildSync({
-              minify: true,
-              bundle: true,
-              entryPoints: [join(process.cwd(), "service-worker.js")],
-              outfile: join(process.cwd(), "dist", "service-worker.js"),
-            });
-          },
-        },
-      ],
+	plugins: [
+		// ...
+		react(),
+		{
+			apply: 'build',
+			enforce: 'post',
+			transformIndexHtml() {
+				buildSync({
+					minify: true,
+					bundle: true,
+					entryPoints: [path.join(process.cwd(), 'src/service-worker.js')],
+					outfile: path.join(process.cwd(), 'dist/service-worker.js'),
+				})
+			},
+		},
+	],
 })
